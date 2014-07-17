6e06376642a676739b8c032ec8386fca5057bded
root <root@lab-test01.kazutan.info>
2014-07-17 15:47:11 +0900

    first commit

---
+++ install_elasticsearch.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cat << EOF > /etc/yum.repos.d/elasticsearch.repo
+[elasticsearch-1.0]
+name=Elasticsearch repository for 1.0.x packages
+baseurl=http://packages.elasticsearch.org/elasticsearch/1.0/centos
+gpgcheck=1
+gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch
+enabled=1
+EOF
+
+yum install -y java
+yum install -y java-1.7.0-openjdk
+yum install -y elasticsearch
+
+service elasticsearch restart
+chkconfig elasticsearch on
+
+curl -X GET http://localhost:9200/
+
+
+yum install -y httpd
+cd /tmp
+wget https://download.elasticsearch.org/kibana/kibana/kibana-3.0.0milestone4.tar.gz
+tar zxvf kibana-3.0.0milestone4.tar.gz
+mv kibana-3.0.0milestone4 /var/www/html/kibana
+
+service httpd restart
+chkconfig httpd on
---
+++ install_fluentd.sh
@@ -0,0 +1,33 @@
+curl -L http://toolbelt.treasuredata.com/sh/install-redhat.sh | sh
+/usr/lib64/fluent/ruby/bin/fluent-gem install fluent-plugin-elasticsearch
+
+cp /etc/td-agent/td-agent.con  /etc/td-agent/td-agent.con.bak
+
+echo << EOF  >> /etc/td-agent/td-agent.con
+
+<source> 
+  type tail 
+  path /var/log/squid/access.log 
+  pos_file /var/log/td-agent/squid-access.log.pos 
+  tag kibana.squid.access 
+  format /^(?<date>[^ ]+)\s+(?<duration>.*) (?<client address>.*) (?<result code>.*) (?<bytes>.*) (?<request method>.*) (?<url>.*) (?<rfc931>.*) (?<hierarchy code>.*) (?<type>.*)$/ 
+  time_format %b %d %H:%M:%S 
+</source>
+
+
+<match kibana.*.*>
+  type elasticsearch
+  type_name access_log
+  host lab-kibana01.kazutan.info
+  port 9200
+  logstash_format true
+  include_tag_key true
+  tag_key @log_name
+</match>
+
+EOF
+
+chmod -R 777 /var/log/squid
+
+service td-agent restart
+chkconfig td-agent on
